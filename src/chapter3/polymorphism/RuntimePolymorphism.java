b28c3decab05355d7703e2edd67dbb00c344d15b
sumit.templatic@gmail.com <aquaris10>
2018-06-22 17:12:04 +0530

    Runtime polymorphism with Dynamic Binding

---
+++ src/chapter3/polymorphism/RuntimePolymorphism.java
@@ -0,0 +1,49 @@
+package chapter3.polymorphism;
+/*
+* Runtime polymorphism with Dynamic Binding
+*
+*
+* */
+class Bank{
+    float getRateOfInterest(){
+        return 0.0f;
+    }
+}
+class SBI extends Bank{
+    @Override
+    float getRateOfInterest() {
+        return 8.7f;
+    }
+}
+
+class AXIS extends Bank{
+    @Override
+    float getRateOfInterest() {
+        return 9.7f;
+    }
+}
+
+class ICICI extends Bank{
+
+    @Override
+    float getRateOfInterest() {
+        return 10.7f;
+    }
+}
+
+
+public class RuntimePolymorphism {
+    public static void main(String[] args) {
+        Bank b;
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("\t\t Runtime Polymorphism ");
+        System.out.println("----------------------------------------------------------------");
+        b = new SBI();//Runtime polymorphism with Dynamic Binding
+        System.out.println("SBI Rate of Interest = \t" + b.getRateOfInterest());
+        b = new AXIS();
+        System.out.println("AXIX Rate of Interest = \t" + b.getRateOfInterest());
+        b = new ICICI();
+        System.out.println("ICICI Rate of Interest =\t" + b.getRateOfInterest());
+        System.out.println("----------------------------------------------------------------");
+    }
+}
