4197cd54773b054f2934dffae75520656f2583d4
sumit.templatic@gmail.com <aquaris10>
2018-06-22 14:58:45 +0530

    Final Keyword Example

--- src/chapter3/polymorphism/FinalKeyword.java
+++ src/chapter3/polymorphism/FinalKeyword.java
@@ -6,19 +6,38 @@ package chapter3.polymorphism;
  * class
  *
  * */
+
+/*final class could be inerited in to any other class*/
 final class Demo {
     void disp() {
-        System.out.println("I am final and I am Can't be override / overload");
+        System.out.println("I am final and I can't be overrided / overloaded by another class.");
     }
 }
 
 
-public class FinalKeyword {
+class Another{
+    final void disp(){
+        System.out.println("I am unmodifiable.");
+    }
+
+}
+
+public class FinalKeyword extends Another{
     final static int data = 100;
 
     public static void main(String[] args) {
         Demo d = new Demo();
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("\t\t Final Class Call ");
+        System.out.println("----------------------------------------------------------------");
         d.disp();
+
+        FinalKeyword f = new FinalKeyword();
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("\t\t Final Method Call  ");
+        System.out.println("----------------------------------------------------------------");
+        f.disp();
+
         //data =1000;   // compile time error
 
     }
