f537203d2968dd4830b17a5ad58903b2bcea918d
sumit.templatic@gmail.com <aquaris10>
2018-06-23 10:46:00 +0530

    Runtime polymorphism with Dynamic Binding

---
+++ src/chapter3/packages/First.java
@@ -0,0 +1,28 @@
+package chapter3.packages;
+
+public class First {
+    protected int b;
+    private int a;
+
+    public static void main(String[] args) {
+        First f = new First();
+        f.meth();
+        f.meth2();
+    }
+
+    /**
+     * This method is procted so it is only used in this package and
+     * only in the child class from the outside packages.
+     */
+    protected void meth() {
+        System.out.println("Hello, I am protected in " + getClass().getSimpleName());
+    }
+
+    /*
+     * this method is private and it is only used within the class body.
+     * outside of the class body it will not be used.
+     * */
+    private void meth2() {
+        System.out.println("Hello, I am private in " + getClass().getSimpleName());
+    }
+}
---
+++ src/chapter3/packages/package1/Second.java
@@ -0,0 +1,12 @@
+package chapter3.packages.package1;
+
+import chapter3.packages.First;
+
+public class Second {
+
+    public static void main(String[] args) {
+        First f = new First();
+//        f.meth();  // method is protected in First class so can't be used
+//        f.meth2(); // method is private in First class so can't be used
+    }
+}
