2113adb6c4a210a8985885171ba743d393c460f0
sumit.templatic@gmail.com <aquaris10>
2018-06-26 11:56:09 +0530

    Thread Updated

--- src/chapter6/Threading.java
+++ src/chapter6/Threading.java
@@ -9,25 +9,137 @@ package chapter6;
  * Terminated
  * */
 
+import java.util.Scanner;
+
 /*
  * For creating thread either you can use Thread class to Extends
  * or you can use Runnable for implements run method.
  * */
 public class Threading extends Thread implements Runnable {
     public static void main(String[] args) {
+        //Method 1 Thread 1
         Threading t1 = new Threading();
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("\t\t Method 1");
-        System.out.println("------------------------------------------------------------------");
-        t1.start();
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("\t\t Method 2");
-        System.out.println("------------------------------------------------------------------");
-        Thread t = new Thread(t1);
-        t.start();
-        System.out.println("------------------------------------------------------------------");
+        //Method 2 Thread 2
+        Threading t2 = new Threading();
+        Thread t = new Thread(t2);
+        //Thread 3
+        Threading t3 = new Threading();
 
+        int choice = 0;
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("\t\t\t Exceptions call");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("Enter the choice Below:\n" +
+                "1. Normal Thread Creation\n" +
+                "2. Directly Running the Threads\n" +
+                "3. Method 1 Joining\n" +
+                "4. Method 2 Joining\n" +
+                "5. Thread with the Name\n" +
+                "6. Thread Priority\n" +
+                "7. Deamon Thread and Thread Group\n");
+        Scanner sc = new Scanner(System.in);
+        choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Method 1 & 2 (Multitasking)");
+                System.out.println("------------------------------------------------------------------");
+                t1.start();
+                t.start();
+                System.out.println("------------------------------------------------------------------");
+                break;
+            case 2:
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Method 1 & 2 (Sequential/without Multitasking)");
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Method(Thread) 1 is running");
+                t1.run();
+                System.out.println("\t\t Method(Thread) 2 is running");
+                t.run();
+                System.out.println("------------------------------------------------------------------");
+                break;
+            case 3:
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Method 1(Thread Joining)");
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Thread 1 (Join) is running");
+                t1.start();
+                try {
+                    /*
+                     * Hold the thread for completing current Thread execution
+                     * */
+                    t1.join();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                System.out.println("\t\t Thread 2 is running");
+                t.start();
+                System.out.println("------------------------------------------------------------------");
+                break;
+            case 4:
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Method 2(Thread Joining)");
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Thread 1 (Join) is running");
+                t1.start();
+                try {
+                    /*
+                     * Hold the thread for 200ms for continuous execution
+                     * */
+                    t1.join(200);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                System.out.println("\t\t Thread 2 is running");
+                t.start();
+                System.out.println("------------------------------------------------------------------");
+                break;
+            case 5:
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Naming the thread");
+                System.out.println("------------------------------------------------------------------");
+                /*
+                 * Thread Name Given Below
+                 * */
+                t1.setName("FirstThread");
+                t.setName("SecondThread");
+                System.out.println("\t\t " + t1.getName() + " is running");
+                t1.start();
+                System.out.println("\t\t " + t.getName() + " is running");
+                t.start();
+                System.out.println("------------------------------------------------------------------");
+                break;
+            case 6:
+                System.out.println("------------------------------------------------------------------");
+                System.out.println("\t\t Thread Priority");
+                System.out.println("------------------------------------------------------------------");
+                /*
+                 * Thread Name Given Below
+                 * */
+                t1.setName("FirstThread");
+                t.setName("SecondThread");
+                t3.setName("ThirdThread");
 
+                /*
+                 * Priority of individual Thread is given Below
+                 * */
+                t1.setPriority(1);  //t1.setPriority(MIN_PRIORITY);
+                t.setPriority(5);   //t.setPriority(NORM_PRIORITY);
+                t3.setPriority(10); //t3.setPriority(MAX_PRIORITY);
+                System.out.println("\t\t " + t1.getName() + " is running");
+                t1.start();
+                System.out.println("\t\t " + t.getName() + " is running");
+                t.start();
+                System.out.println("\t\t " + t3.getName() + " is running");
+                t3.start();
+                System.out.println("------------------------------------------------------------------");
+                break;
+            case 7:
+                break;
+            default:
+                System.err.println("Wrong Choice");
+                break;
+        }
     }
 
     /*
@@ -42,7 +154,14 @@ public class Threading extends Thread implements Runnable {
                  * It will make the thread into sleep mode for 100ms
                  * */
                 Thread.sleep(100);
-                System.out.println("i = " + i);
+                /*
+                 * For getting current thread information
+                 * */
+                System.out.println("Thread Name : " + Thread.currentThread().getName() + "\n" +
+                        "Thread Priority :" + Thread.currentThread().getPriority() + "\n" +
+                        "output : i = " + i);
+                System.out.println("------------------------------------------------------------------");
+
             }
         } catch (Exception e) {
             System.out.println(e);
