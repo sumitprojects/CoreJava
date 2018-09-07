4d0d47eb8e13e03c5385bb50776791a3cb78993f
sumit.templatic@gmail.com <aquaris10>
2018-09-07 18:41:15 +0530

    New palindrome checker with generator

--- src/NewPalindrome.java
+++ src/NewPalindrome.java
@@ -1,13 +1,10 @@
 public class NewPalindrome {
-    public static void main (String[] args) {
-        Boolean on = false;
-        String number = "sumus";
-        Integer start = 0, end, mid;
-        end = number.length();
-        mid = (end / 2);
+    static Boolean palindromeChecker (String data) {
+        char array[] = data.toLowerCase().toCharArray();
+        int start = 0, end = data.length(), mid = (end / 2);
         --end;
+        boolean on = false;
         while (mid > 0) {
-            char array[] = number.toCharArray();
             if (array[start] == array[end]) {
                 ++start;
                 --end;
@@ -19,10 +16,39 @@ public class NewPalindrome {
                 break;
             }
         }
-        if (on) {
+        return on;
+    }
+    
+    static String palindromeGenerator (String data) {
+        char array[] = data.toLowerCase().toCharArray();
+        int start = 0, end = data.length(), mid = (end / 2);
+        --end;
+        boolean on = false;
+        while (mid > 0) {
+            if (array[start] == array[end]) {
+                on = true;
+            } else {
+                int min = Integer.min(array[start], array[end]);
+                array[start] = (char) min;
+                array[end] = (char) min;
+                on = true;
+            }
+            ++start;
+            --end;
+            --mid;
+        }
+        return String.valueOf(array);
+    }
+    
+    public static void main (String[] args) {
+        boolean on = false;
+        String number = "mon";
+        
+        if (palindromeChecker(number)) {
             System.out.println("Data is palindrome");
         } else {
             System.out.println("Data is not palindrome");
+            System.out.println("Nearest Palindrome Data : " + palindromeGenerator(number));
         }
         
     }
