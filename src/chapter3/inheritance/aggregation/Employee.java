7c1b95a601d6c5673cb6a3f9e1483ae485c89832
sumit.templatic@gmail.com <aquaris10>
2018-09-12 15:54:36 +0530

    function updated

--- src/chapter3/inheritance/aggregation/Employee.java
+++ src/chapter3/inheritance/aggregation/Employee.java
@@ -14,9 +14,10 @@ public class Employee {
         Address a2 = new Address("NVS", "GUJ", "IND");
         Person p1 = new Person("Tirth", "Lakhani", "R", a1);
         Person p2 = new Person("Om", "Sonani", "K", a2);
+      Person p3 = new Person("Akshay", "Soni", "R", a1);
         Employee e1 = new Employee(p1, 20000f);
         Employee e2 = new Employee(p2, 30000f);
-
+      Employee e3 = new Employee(p3, 35000f);
         for (int i = 0; i < 50; i++) {
             System.out.print("-");
         }
@@ -24,6 +25,7 @@ public class Employee {
         System.out.println("\tFULL NAME\t \t ADDRESS \t \t SALARY \t");
         e1.Display();
         e2.Display();
+      e3.Display();
     }
 
     public void Display() {
