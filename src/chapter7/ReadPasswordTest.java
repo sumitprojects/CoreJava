6de8c8df6275e903df0fc890e45ce0c94b85d5f4
sumit.templatic@gmail.com <aquaris10>
2018-10-09 12:17:21 +0530

    Console Reader

--- src/chapter7/ReadPasswordTest.java
+++ src/chapter7/ReadPasswordTest.java
@@ -4,15 +4,35 @@ import java.io.Console;
 
 public class ReadPasswordTest {
 	public static void main (String args[]) {
-		String pass;
-		Console c = System.console();
-		String text = c.readLine("%s", "Enter your name:\n");
-		pass = String.valueOf(c.readPassword("%s", "Enter password:\n"));
-		System.out.println("Name is: " + text + "\nPassword is: " + pass);
-		if (text.equals("sumit") && pass.equals("sumit123")) {
-			System.out.println("You are authorised user");
-		} else {
-			System.err.println("You are not authorised");
+		String text = null, pass = null;
+		try {
+			Console c = System.console();
+			try {
+				text = c.readLine("%s", "Enter your name:\n");
+				pass = String.valueOf(c.readPassword("%s", "Enter password:\n"));
+			} catch (Exception e) {
+				System.err.println("Error: You have mot run the program on console.");
+				text = "";
+				pass = "";
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Name is: " + text + "\nPassword is: " + pass);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (text.equals("sumit") && pass.equals("sumit123")) {
+				System.out.println("You are authorised user");
+			} else {
+				System.err.println("You are not authorised");
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
 	}
 }
