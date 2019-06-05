78fa474a86170e282cb361d091e33046281d374c
sumit.templatic@gmail.com <aquaris10>
2019-06-05 11:54:44 +0530

    New Code Updated

--- src/ThreadingConcepts.java
+++ src/ThreadingConcepts.java
@@ -1,23 +1,55 @@
-public class ThreadingConcepts extends Thread {
+public class ThreadingConcepts implements Runnable {
+	int sum;
 	public static void main (String[] args) {
-		ThreadingConcepts th = new ThreadingConcepts();
-		ThreadingConcepts th2 = new ThreadingConcepts();
-		ThreadingConcepts th3 = new ThreadingConcepts();
-		th.setName("process 1");
-		th2.setName("process 2");
-		th3.setName("process 3");
+		Thread th = new Thread(new ThreadingConcepts(), "process 1");
+		Thread th2 = new Thread(new ThreadingConcepts(), "process 2");
+		Thread th3 = new Thread(new ThreadingConcepts(), "process 3");
+		th3.start();
+		try {
+			th3.join(500);
+		} catch (Exception e) {
+		
+		}
 		th.start();
 		th2.start();
-		th3.start();
-		System.out.println("hello");
+		
+		th.setPriority(1);
+		th2.setPriority(5);
+		th3.setPriority(10);
+		
+		
+		try {
+			Thread.sleep(10000);
+			System.out.println("hello");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void run () {
 		try {
-			for (int i = 1; i <= 5; i++) {
-				Thread.sleep(1000);
-				System.out.println(Thread.currentThread().getName() + " -" + i);
+			Thread.sleep(1000);
+			if (Thread.currentThread().getPriority() == Thread.MAX_PRIORITY) {
+				for (int i = 11; i <= 20; i++) {
+					Thread.sleep(Thread.MAX_PRIORITY * 50);
+					this.sum += i;
+					System.out.println(Thread.currentThread().getName() + " -" + this.sum);
+				}
+			}
+			if (Thread.currentThread().getPriority() == Thread.NORM_PRIORITY) {
+				for (int i = 1; i <= 10; i++) {
+					Thread.sleep(Thread.NORM_PRIORITY * 150);
+					this.sum += i;
+					System.out.println(Thread.currentThread().getName() + " -" + this.sum);
+				}
+			}
+			if (Thread.currentThread().getPriority() == Thread.MIN_PRIORITY) {
+				for (int i = 21; i <= 30; i++) {
+					Thread.sleep(Thread.MIN_PRIORITY * 550);
+					this.sum += i;
+					System.out.println(Thread.currentThread().getName() + " -" + this.sum);
+				}
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
