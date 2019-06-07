541c8c26a10b8df8903ab30f47a65818b16cef7c
sumit.templatic@gmail.com <aquaris10>
2019-06-07 16:55:38 +0530

    New Code Updated

--- src/Threadings/RuntimeDemo.java
+++ src/Threadings/RuntimeDemo.java
@@ -8,6 +8,7 @@ public class RuntimeDemo {
 		try {
 			Runtime runtime = Runtime.getRuntime();
 			runtime.exec("cmd /c start cmd.exe /K \"tree && ping 192.168.0.1 -t  \"");
+			//runtime.exec("/usr/bin/gnome-terminal -e \"ls -l\"");
 		} catch (IOException e) {
 			System.out.println(e);
 		}
